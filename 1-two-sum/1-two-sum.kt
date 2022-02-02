b28813a8d75112d7a53999859732063f73732148
Wonjun <aa3991@office.uc.ac.kr>
2022-02-02 21:47:02 +0900

    Time: 256 ms (70.39%), Space: 37.1 MB (89.37%) - LeetHub

---
+++ 1-two-sum/1-two-sum.kt
@@ -0,0 +1,14 @@
+class Solution {
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        val numsCount = nums.count()
+        for (first in 0 until numsCount) {
+            for (second in first + 1 until numsCount) {
+                var sum = nums[first] + nums[second]
+                if( sum == target){
+                    return intArrayOf(first, second)
+                }
+            }
+        }
+        return intArrayOf(0, 0)
+    }
+}
