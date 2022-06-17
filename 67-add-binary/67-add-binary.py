8ca5863d89514d28d8d3eb14e0831d670996f7a8
Wonjun <aa3991@office.uc.ac.kr>
2022-06-17 23:47:38 +0900

    Time: 65 ms (17.54%), Space: 14 MB (23.97%) - LeetHub

--- 67-add-binary/67-add-binary.py
+++ 67-add-binary/67-add-binary.py
@@ -1,5 +1,4 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        result = int(a, 2) + int(b, 2)
-        return bin(result)[2:]
+        return bin(int(a, 2) + int(b, 2))[2:]
         
