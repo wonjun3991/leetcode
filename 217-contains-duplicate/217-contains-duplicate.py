7170a6a2836d07ea2c94863eb8b12e1f531217c0
Wonjun <aa3991@office.uc.ac.kr>
2022-07-11 23:13:37 +0900

    Time: 645 ms (52.82%), Space: 25.9 MB (93.27%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -0,0 +1,10 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        dic = {}
+        for i in nums:
+            if i in dic:
+                return True
+            dic[i] = 1
+        return False
+            
+        
