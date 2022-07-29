5b13e17eb22f0b3a2867183a07aebc679eff82e2
Wonjun <aa3991@office.uc.ac.kr>
2022-07-29 16:58:07 +0900

    Time: 83 ms (77.50%), Space: 43.3 MB (91.67%) - LeetHub

---
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string[]} words
+ * @param {string} pattern
+ * @return {string[]}
+ */
+var findAndReplacePattern = function(words, pattern) {
+    const alpha = Array.from(Array(26)).map((e, i) => i + 97);
+    const alphabet = alpha.map((x) => String.fromCharCode(x));
+    console.log(alphabet)
+    
+    const patternCatch = function(pattern){
+        const mapList = pattern.split('').reduce(function(prev, curr){
+            if(!prev.includes(curr)){
+                prev.push(curr)
+            }
+            return prev
+        }, [])
+        return pattern.split('').map((char)=>alphabet[mapList.indexOf(char)]).join('')
+    }
+    
+    const result = patternCatch(pattern)
+        
+    return words.filter((word)=>patternCatch(word) == result)
+};
