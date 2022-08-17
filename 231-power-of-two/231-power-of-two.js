71f73fcb0dafa1a7baeac06e292bdd46889d4f64
Wonjun <aa3991@office.uc.ac.kr>
2022-08-18 01:19:46 +0900

    Time: 85 ms (82.35%), Space: 43.7 MB (40.67%) - LeetHub

---
+++ 231-power-of-two/231-power-of-two.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfTwo = function(n) {
+    if (n== 0) return false;
+    
+    while(n % 2 == 0){
+        n = n / 2
+        
+    }
+    if( n == 1){
+        return true;
+    }
+    
+    return false;
+};
