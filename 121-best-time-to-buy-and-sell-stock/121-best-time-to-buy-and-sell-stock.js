d654cf36b4cbedeb24db9ab3bf2f53765b461018
Wonjun <aa3991@office.uc.ac.kr>
2022-08-18 23:36:33 +0900

    Time: 90 ms (92.21%), Space: 51.8 MB (55.26%) - LeetHub

---
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let min = Math.max(...prices);
+    let max = 0;
+    for(let i = 0; i< prices.length; i++){
+        if(prices[i] < min){
+            min = prices[i]
+        }
+        else if (prices[i] - min > max){
+            max = prices[i]  - min
+        }
+    }
+    return max;
+};
