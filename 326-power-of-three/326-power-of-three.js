1405dcdf406ff0f52e1fc8658ee97b0a187f5270
Wonjun <aa3991@office.uc.ac.kr>
2022-08-21 06:31:13 +0900

    Time: 315 ms (61.00%), Space: 50.8 MB (89.14%) - LeetHub

---
+++ 326-power-of-three/326-power-of-three.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfThree = function(n) {
+    if(n == 1){
+        return true;
+    }
+    
+    if(n<3){
+        return false;
+    }
+    
+    while(n % 3 == 0){
+        n = n /3
+    }
+    return n == 1 ? true : false
+};
