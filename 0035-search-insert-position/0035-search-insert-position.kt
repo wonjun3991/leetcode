e1ce7e04c5066e3767faebb15b91f781ec906dd5
Wonjun <aa3991@office.uc.ac.kr>
2022-10-23 16:45:00 +0900

    Time: 313 ms (57.22%), Space: 37.7 MB (74.39%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.kt
+++ 0035-search-insert-position/0035-search-insert-position.kt
@@ -1,13 +1,22 @@
 class Solution {
     fun searchInsert(nums: IntArray, target: Int): Int {
-        var output = 0
-        for((index, value) in nums.withIndex()){
-            if(target <= value){
-                break
+        var pivot = 0
+        var left = 0
+        var right = nums.size - 1
+        
+        while(left <= right){
+            pivot = left + (right - left) / 2
+            if(nums[pivot] == target){
+                return pivot
             }
-            output = index + 1
+            
+            if(target < nums[pivot]){
+                right = pivot - 1
+            } else{
+                left = pivot + 1
+            }
         }
         
-        return output
+        return left
     }
 }
