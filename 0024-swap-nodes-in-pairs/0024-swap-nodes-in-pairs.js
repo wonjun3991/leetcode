8e2b7d9d1cb06520388c100eb2fd0b9af699ccf8
Wonjun <aa3991@office.uc.ac.kr>
2023-05-16 23:43:34 +0900

    Time: 61 ms (42.56%), Space: 41.9 MB (82.08%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.js
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.js
@@ -10,16 +10,14 @@
  * @return {ListNode}
  */
 var swapPairs = function(head) {
-  // node가 끝날때 까지 2개씩 노드를 조회한다
     let cursor = head;
-    console.log(head);
+    
     while(cursor !== null && cursor.next !== null){
         let temp = cursor.val;
         cursor.val = cursor.next.val;
         cursor.next.val = temp;
         cursor = cursor.next.next;
     }
-    // 노드가 두개가 아니라면 취소한다
-    // 노드가 두개면 두개의 노드의 val를 변경한다
+
     return head;
 };
