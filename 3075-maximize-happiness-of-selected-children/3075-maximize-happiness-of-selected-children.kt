98f5126102936010c7e10ac782b50deb5b398759
Wonjun <cwj0964@gmail.com>
2024-05-10 01:51:10 +0900

    Time: 590 ms (98.80%), Space: 73.4 MB (63.86%) - LeetHub

--- 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.kt
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.kt
@@ -1,16 +1,21 @@
-import java.util.PriorityQueue
 
 class Solution {
     fun maximumHappinessSum(happiness: IntArray, k: Int): Long {
-        val pq = PriorityQueue<Int> { a, b -> b.compareTo(a) }
-        happiness.forEach { pq.add(it) }
+        happiness.sort()
+        val n = happiness.size
+        var i = n - 1
         var sum = 0L
-        for(i in 0 until k){
-            val poll = pq.poll()
-            if(poll - i < 0){
-                continue
-            }
-            sum = sum + poll - i
+
+        while (i >= 0) {
+            val offset = n - 1 - i
+
+            val score = maxOf(0, (happiness[i] - offset))
+            sum += score
+
+            val collected = n - i
+            if (score == 0 || collected == k) break //we cannot score more, lower elements will be 0 only
+
+            i--
         }
 
         return sum
