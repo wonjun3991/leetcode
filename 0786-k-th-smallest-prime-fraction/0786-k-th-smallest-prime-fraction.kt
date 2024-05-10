6feacb825f601fc5e22f11fe1c80f6fbc0860651
Wonjun <cwj0964@gmail.com>
2024-05-10 20:36:56 +0900

    Time: 390 ms (50.00%), Space: 38.6 MB (50.00%) - LeetHub

--- 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.kt
+++ 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.kt
@@ -1,23 +1,49 @@
-import java.util.PriorityQueue
 
-class Solution {
+import java.util.*
+
+internal class Solution {
     fun kthSmallestPrimeFraction(arr: IntArray, k: Int): IntArray {
-        val pq = PriorityQueue<Pair<Int, Int>>
-        { a, b -> (a.first * b.second).compareTo(a.second * b.first ) }
+        // Create a priority queue to store pairs of fractions
+        var k = k
+        val pq = PriorityQueue { a: DoubleArray, b: DoubleArray ->
+            java.lang.Double.compare(
+                b[0],
+                a[0]
+            )
+        }
 
-        for(i in arr.indices){
-            for(j in i + 1 until arr.size){
-                pq.add(arr[i] to arr[j])
+        // Push the fractions formed by dividing each element by
+        // the largest element into the priority queue
+        for (i in arr.indices) {
+            pq.offer(
+                doubleArrayOf(
+                    -1.0 * arr[i] / arr[arr.size - 1],
+                    i.toDouble(),
+                    (arr.size - 1).toDouble()
+                )
+            )
+        }
+
+        // Iteratively remove the top element (smallest fraction) 
+        // and replace it with the next smallest fraction
+        while (--k > 0) {
+            val cur = pq.poll()
+            val numeratorIndex = cur[1].toInt()
+            val denominatorIndex = cur[2].toInt() - 1
+            if (denominatorIndex > numeratorIndex) {
+                pq.offer(
+                    doubleArrayOf(
+                        -1.0 * arr[numeratorIndex] / arr[denominatorIndex],
+                        numeratorIndex.toDouble(),
+                        denominatorIndex.toDouble()
+                    )
+                )
             }
         }
-        var count = k
-        while(count != 1) {
-            pq.poll()
-            count = count - 1
-        }
 
-        val poll = pq.poll()
-
-        return intArrayOf(poll.first, poll.second)
+        // Retrieve the kth smallest fraction from 
+        // the top of the priority queue
+        val result = pq.poll()
+        return intArrayOf(arr[result[1].toInt()], arr[result[2].toInt()])
     }
 }
