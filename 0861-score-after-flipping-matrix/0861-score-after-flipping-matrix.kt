344748e8bc291d45885eabd67cfd47869d22165b
Wonjun <cwj0964@gmail.com>
2024-06-01 23:37:25 +0900

    Time: 144 ms (85.05%), Space: 34.4 MB (66.36%) - LeetHub

--- 0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.kt
+++ 0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.kt
@@ -1,48 +1,43 @@
 import kotlin.math.abs
-import kotlin.math.pow
 
 class Solution {
     fun matrixScore(grid: Array<IntArray>): Int {
         val height = grid.size
-        val width =  grid[0].size
+        val width = grid[0].size
         // 가로로 먼저 뒤집고
-        for(i in 0 until height){
-            if(grid[i][0] == 0){
-                for(l in 0 until  width){
+        for (i in 0 until height) {
+            if (grid[i][0] == 0) {
+                for (l in 0 until width) {
                     grid[i][l] = abs(grid[i][l] - 1)
                 }
             }
         }
-        
-        println(grid.map{ it.joinToString(",")}.joinToString("//"))
 
         // 세로로 뒤집고
-        for(w in 0 until width){
+        for (w in 0 until width) {
             var zeroCount = 0
-            for(h in 0 until  height){
-                if(grid[h][w] == 0){
+            for (h in 0 until height) {
+                if (grid[h][w] == 0) {
                     zeroCount++
                 }
             }
 
-            if(zeroCount > height - zeroCount){
-                for(h in 0 until  height){
+            if (zeroCount > height - zeroCount) {
+                for (h in 0 until height) {
                     grid[h][w] = abs(grid[h][w] - 1)
                 }
             }
         }
-        
-        println(grid.map{ it.joinToString(",")}.joinToString("//"))
-
         // 합 계산
-        var score:Double = 0.toDouble()
-        for(i in 0 until height){
-            for(l in 0 until  width){
-                if(grid[i][l] == 1){
-                    score += 2.toDouble().pow(width - 1 - l)    
-                }
+        var score = 0
+        for (i in 0 until height) {
+            for (l in 0 until width) {
+                score += grid[i][l] shl (width - 1 - l)
             }
         }
-        return score.toInt()
+
+
+
+        return score
     }
 }
