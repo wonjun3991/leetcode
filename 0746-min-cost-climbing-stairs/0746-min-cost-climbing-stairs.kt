53862e572091e2f09cce97ba0645df8aa3a31019
최원준 (Wonjun Choi) <cwj0964@gmail.com>
2024-11-07 00:02:20 +0900

    Time: 1 ms (100.00%), Space: 38.1 MB (15.35%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.kt
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.kt
@@ -1,25 +1,24 @@
-import kotlin.math.max
 import kotlin.math.min
 
 class Solution {
     fun minCostClimbingStairs(cost: IntArray): Int {
-        val N = cost.size
+        val size = cost.size
 
-        if (N == 0) {
+        if (size == 0) {
             return 0
         }
 
-        val maxRobbedAmount = IntArray(cost.size + 1)
+        val dynamicTable = IntArray(cost.size + 1)
 
-        maxRobbedAmount[N] = 0
-        maxRobbedAmount[N - 1] = cost[N - 1]
+        dynamicTable[size] = 0
+        dynamicTable[size - 1] = cost[size - 1]
 
-        for (i in N - 2 downTo 0) {
-            maxRobbedAmount[i] = min(
-                maxRobbedAmount[i + 1] + cost[i],
-                maxRobbedAmount[i + 2] + cost[i]
+        for (i in size - 2 downTo 0) {
+            dynamicTable[i] = min(
+                dynamicTable[i + 1] + cost[i],
+                dynamicTable[i + 2] + cost[i]
             )
         }
-        return min(maxRobbedAmount[0], maxRobbedAmount[1])
+        return min(dynamicTable[0], dynamicTable[1])
     }
 }
