b8a530061ce445c2c765c7b4632b317486d9ce4d
최원준 (Wonjun Choi) <cwj0964@gmail.com>
2024-12-19 23:25:35 +0900

    Time: 0 ms (100%), Space: 12.3 MB (11.27%) - LeetHub

---
+++ 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def maxChunksToSorted(self, arr):
+        max_so_far = 0
+        chunks = 0
+
+        for i, num in enumerate(arr):
+            max_so_far = max(max_so_far, num)
+            if max_so_far == i:
+                chunks += 1
+        
+        return chunks
