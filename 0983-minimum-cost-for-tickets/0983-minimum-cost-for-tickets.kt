93b65aaa1980446099c1d701fda351e5b5ead5c8
최원준 (Wonjun Choi) <cwj0964@gmail.com>
2024-12-31 22:56:27 +0900

    Time: 1 ms (100%), Space: 35.4 MB (84.85%) - LeetHub

---
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.kt
@@ -0,0 +1,27 @@
+import kotlin.math.max
+import kotlin.math.min
+
+class Solution {
+    fun mincostTickets(days: IntArray, costs: IntArray): Int {
+        val lastDay = days[days.size - 1]
+        val dp = IntArray(lastDay + 1) { 0 }
+
+        var i = 0
+        for (day in 1..lastDay) {
+            if (day < days[i]) {
+                dp[day] = dp[day - 1]
+            } else {
+                i++
+                dp[day] = min(
+                    dp[day - 1] + costs[0],
+                    min(
+                        dp[max(0, day - 7)] + costs[1],
+                        dp[max(0, day - 30)] + costs[2]
+                    )
+                )
+            }
+        }
+
+        return dp[lastDay]
+    }
+}
