8ca0425a9a9daa18a65cd80d48d454aad29148a4
최원준 (Wonjun Choi) <cwj0964@gmail.com>
2025-01-17 18:21:05 +0900

    Time: 21 ms (50%), Space: 79.6 MB (50%) - LeetHub

--- 2683-neighboring-bitwise-xor/2683-neighboring-bitwise-xor.kt
+++ 2683-neighboring-bitwise-xor/2683-neighboring-bitwise-xor.kt
@@ -1,10 +1,1 @@
-class Solution {
-    fun doesValidArrayExist(derived: IntArray): Boolean {
-        var bitCount = 0
-        for (i in 0 until derived.size - 1) {
-            if(derived[i] == 1) bitCount++
-        }
-
-        return bitCount % 2 == derived.last()
-    }
-}
+class Solution { fun doesValidArrayExist(derived: IntArray) = derived.sum() % 2 == 0 }
