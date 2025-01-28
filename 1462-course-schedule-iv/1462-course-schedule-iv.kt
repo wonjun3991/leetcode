f708fd1bae7cbf2faefc1215fa7690b09895e7f1
최원준 (Wonjun Choi) <cwj0964@gmail.com>
2025-01-28 18:55:37 +0900

    Time: 89 ms (20%), Space: 57.4 MB (80%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.kt
+++ 1462-course-schedule-iv/1462-course-schedule-iv.kt
@@ -1,50 +1,41 @@
 import java.util.*
-import kotlin.collections.HashSet
 
 class Solution {
     fun checkIfPrerequisite(numCourses: Int, prerequisites: Array<IntArray>, queries: Array<IntArray>): List<Boolean> {
-        val adjList = Array(numCourses) { mutableListOf<Int>() }
-        val indegreeCount = Array(numCourses) { 0 }
+        val adjacencyList: Array<MutableList<Int>> = Array(numCourses) { mutableListOf() }
+        val inDegreeCountList: Array<Int> = Array(numCourses) { 0 }
 
         prerequisites.forEach {
-            adjList[it[0]].add(it[1])
-            indegreeCount[it[1]]++
+            adjacencyList[it[0]].add(it[1])
+            inDegreeCountList[it[1]]++
         }
 
-        val queue = LinkedList<Int>()
-        for (i in 0 until numCourses) {
-            if (indegreeCount[i] == 0) {
+        val queue: Queue<Int> = LinkedList<Int>()
+
+        for (i in inDegreeCountList.indices) {
+            if (inDegreeCountList[i] == 0) {
                 queue.add(i)
             }
         }
 
-        val connectedListMap = hashMapOf<Int, MutableSet<Int>>()
+        val inDegreeConnectedList: Array<MutableSet<Int>> = Array(numCourses) { mutableSetOf() }
 
         while (queue.isNotEmpty()) {
-            // 진입차수 0 인 Node
-            val current = queue.removeFirst()
+            val cur = queue.remove()!!
 
-            // 0인 Node와 연결된 Node 목록
-            adjList[current].forEach { adjacency ->
-                // 연결된 Node에 current와 연결되었음을 명시
-                connectedListMap.computeIfAbsent(adjacency) { HashSet() }.add(current)
-
-                // current에 진입하는 Node들에도 해당 Node 들과 연결되었음을 명시
-                connectedListMap.getOrDefault(current, HashSet()).forEach{
-                    connectedListMap.get(adjacency)!!.add(it)
+            adjacencyList[cur].forEach { adjacency ->
+                inDegreeConnectedList[adjacency].add(cur)
+                inDegreeConnectedList[cur].forEach{
+                    inDegreeConnectedList[adjacency].add(it)
                 }
+                inDegreeCountList[adjacency]--
 
-                // 진입차수 차감
-                indegreeCount[adjacency]--
-
-                if(indegreeCount[adjacency] == 0){
+                if(inDegreeCountList[adjacency] == 0) {
                     queue.add(adjacency)
                 }
             }
         }
 
-        return queries.map {
-            connectedListMap.getOrDefault(it[1], HashSet()).contains(it[0])
-        }.toList()
+        return queries.map{ inDegreeConnectedList[it[1]].contains(it[0])}.toList()
     }
 }
