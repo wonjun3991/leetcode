4c3a221a5f5517afb76026c64abdc8d60a6faf6c
최원준 (Wonjun Choi) <cwj0964@gmail.com>
2025-07-03 21:59:51 +0900

    Time: 179 ms (5.56%), Space: 46.3 MB (27.78%) - LeetHub

--- 3304-find-the-k-th-character-in-string-game-i/3304-find-the-k-th-character-in-string-game-i.kt
+++ 3304-find-the-k-th-character-in-string-game-i/3304-find-the-k-th-character-in-string-game-i.kt
@@ -2,6 +2,7 @@ class Solution {
     fun kthCharacter(k: Int): Char {
         var s = listOf('a')
         while (s.lastIndex < k - 1) s += s.map { it + 1 }
+        println(s)
         return s[k - 1]
     }
 }
