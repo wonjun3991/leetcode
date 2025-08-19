013b9a4b979bbbed77712b4743141d77a377d880
최원준 (Wonjun Choi) <cwj0964@gmail.com>
2025-08-19 09:52:55 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.kt
@@ -0,0 +1,19 @@
+class Solution {
+    fun zeroFilledSubarray(nums: IntArray): Long {
+        var result = 0L
+        var count = 0
+        nums.fold(count) { acc, i ->
+            if(i ==0){
+                count += 1
+            } else{
+                result += count * (count + 1) / 2
+                count = 0
+            }
+            
+            return@fold count
+        }
+        result += count * (count + 1) / 2
+
+        return result
+    }
+}
