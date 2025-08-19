e86de68227cbbb47697dbd68480f208f2e30ba9a
최원준 (Wonjun Choi) <cwj0964@gmail.com>
2025-08-19 09:58:38 +0900

    Time: 5 ms (18.18%), Space: 63 MB (72.73%) - LeetHub

--- 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.kt
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.kt
@@ -1,15 +1,15 @@
 class Solution {
     fun zeroFilledSubarray(nums: IntArray): Long {
         var result = 0L
-        var count = 0
+        var count = 0L
         nums.fold(count) { acc, i ->
-            if(i ==0){
+            if (i == 0) {
                 count += 1
-            } else{
+            } else {
                 result += count * (count + 1) / 2
                 count = 0
             }
-            
+
             return@fold count
         }
         result += count * (count + 1) / 2
